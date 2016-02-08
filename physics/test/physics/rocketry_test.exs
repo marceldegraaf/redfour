ff93f488e6d114b9f084aa5e29f6f264fecd726d
Marcel de Graaf <mail@marceldegraaf.net>
2016-02-08 11:49:37 +0100

    added test case for atom function

--- physics/test/physics/rocketry_test.exs
+++ physics/test/physics/rocketry_test.exs
@@ -2,6 +2,10 @@ defmodule PhysicsRocketryTest do
   use ExUnit.Case
   doctest Physics.Rocketry
 
+  test "earth's escape velocity" do
+    assert Physics.Rocketry.escape_velocity(:earth) == 11.2
+  end
+
   test "escape velocity" do
     v = %{mass: 5.972e24, radius: 6.371e6}
       |> Physics.Rocketry.escape_velocity
